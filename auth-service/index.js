5d18e66985ebcac95ad2bbeaae11f9211a466305
Rico Harsono <barkeley.rico@gmail.com>
2024-11-08 14:37:42 +0800

    initial commit

---
+++ auth-service/index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const dotenv = require("dotenv");
+const User = require("./util/userModel");
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  console.log("test successful");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userExists = await User.findUser(username);
+    if (userExists)
+      return res.status(400).json({ message: "Username already taken" });
+
+    const result = await User.createUser(username, password);
+    return res.status(201).json({
+      message: "User created successfully!",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "An error occurred." });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findUser(username);
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const validPassword = await bcrypt.compare(password, user.Password);
+    if (!validPassword)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    if (user.isActive !== "1")
+      return res
+        .status(400)
+        .json({ message: "Please ask admins to activate the account!" });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "4d",
+    });
+    res.status(200).json({ token, isActive: user.isActive });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "An error occurred during login." });
+  }
+});
+
+app.post("/changepassword", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findUser(username);
+    if (!user) return res.status(400).json({ message: "Username not found" });
+    await User.changePassword(username, password);
+    return res.status(200).json({ message: "Successful" });
+  } catch (error) {
+    console.error("Password change error:", error);
+    return res.status(500).json({ error: "An error occurred during login." });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ auth-service/util/userModel.js
@@ -0,0 +1,33 @@
+const connection = require("./db");
+const bcrypt = require("bcryptjs");
+
+class User {
+  static async createUser(username, password) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const [result] = await connection.query(
+      "INSERT INTO userlist VALUES (null, ?, ?, ?, ?)",
+      [username, hashedPassword, "False", "False"]
+    );
+    return result;
+  }
+  static async findUser(username) {
+    const [result] = await connection.query(
+      "SELECT * FROM userlist WHERE Username = ?",
+      [username]
+    );
+    return result[0];
+  }
+  static async changePassword(username, password) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const [result] = await connection.query(
+      "UPDATE userlist SET Password = ?, isActive = 0 WHERE Username = ?",
+      [hashedPassword, username]
+    );
+    // Check if any rows were affected
+    if (result.affectedRows === 0) {
+      throw new Error("Password update failed");
+    }
+  }
+}
+
+module.exports = User;
